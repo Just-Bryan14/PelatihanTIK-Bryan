9adee784732f495fed37ed79600947f72080b15c
Just-Bryan14 <nabilganteng8242@gmail.com>
2025-10-21 13:22:32 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const tombolSapa = document.getElementByid('sapaButton);
+                                           
+tombolSapa.addEventlistener('click', function() {
+  alert('Halo! Terima Kasih sudah berkunjung!');
+});  
